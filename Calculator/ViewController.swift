fc213f2b0c0783703721db507a61a9ff58cd9575
oxovu <vlada318@live.com>
2018-07-10 18:43:31 +0300

    some beauty changes

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,16 +14,45 @@ class ViewController: UIViewController {
     var intNumberOnScreen:Int = 0
     var previousNumber:Double = 0
     var math = false
+    var dot = false
+    var done = false
     var operation = 0
     
     @IBOutlet var back: UIView!
     @IBOutlet weak var result: UILabel!
     
     @IBAction func point(_ sender: Any) {
-        result.text = result.text! + "."
+        if !dot {
+            if math {
+                result.text = "0"
+                math = false
+            }
+            result.text = result.text! + "."
+            dot = true
+        }
     }
 
-    @IBAction func numbers(_ sender: UIButton) {
+    @IBAction func clear(_ sender: UIButton) {
+        result.text = "0"
+        previousNumber = 0
+        numberOnScreen = 0
+        operation = 0
+        dot = false
+    }
+    
+    @IBAction func numbers(_ sender: UIButton) { // учитывать переполнение
+//        if done {
+//            result.text = ""
+//            numberOnScreen = 0
+//            previousNumber = 0
+//            math = false
+//            operation = 0
+//            done = false
+//        }
+        if result.text!.characters.count < 13 { //разберись с предупреждением
+        if result.text == "0" {
+            result.text = ""
+        }
         if math {
             result.text = String(sender.tag - 1)
             numberOnScreen = Double(result.text!)!
@@ -38,12 +67,15 @@ class ViewController: UIViewController {
         } else {
             back.backgroundColor = UIColor(red: 0.26, green: 0.26, blue: 0.26, alpha: 1.0)
             result.textColor = UIColor.white
+            }
         }
     }
     
     @IBAction func percent(_ sender: UIButton) {
-        if sender.tag == 13 { //%
-            numberOnScreen = previousNumber * numberOnScreen / 100
+        if previousNumber == 0 {
+            numberOnScreen = numberOnScreen / 100
+        } else {
+        numberOnScreen = previousNumber * numberOnScreen / 100
         }
         if (numberOnScreen.truncatingRemainder(dividingBy: 1)) == 0{
             intNumberOnScreen = Int(numberOnScreen)
@@ -53,17 +85,22 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func operations(_ sender: UIButton) {
+    @IBAction func plusMinus(_ sender: UIButton) {
+        numberOnScreen = -numberOnScreen
+        result.text = String(numberOnScreen)
+    
+    }
+    
+    @IBAction func operations(_ sender: UIButton) { // сделать, чтобы при нескольких нажатиях на операторы, выводился промежуточный результат
+        dot = false
         if result.text != "" && sender.tag != 11 && sender.tag != 18 {
             operation = sender.tag
             previousNumber = Double(result.text!)!
             math = true
-            if operation == 12 { //±
-                result.text = String(numberOnScreen * (-1))
-            }
         } else if sender.tag == 18 {
             if operation == 14 { //divide
                 numberOnScreen = previousNumber / numberOnScreen
+                numberOnScreen = round(numberOnScreen * pow(10, 11)) / pow(10, 11)
             }
             if operation == 15 { //multiply
                 numberOnScreen = previousNumber * numberOnScreen
@@ -80,11 +117,7 @@ class ViewController: UIViewController {
             } else{
                 result.text = String(numberOnScreen)
             }
-        } else if sender.tag == 11 {
-            result.text = "0"
-            previousNumber = 0
-            numberOnScreen = 0
-            operation = 0
+//            done = true
         }
         if result.text == String(666){
             back.backgroundColor = UIColor(red: 0.9, green: 0.2, blue: 0.2, alpha: 1.0)
